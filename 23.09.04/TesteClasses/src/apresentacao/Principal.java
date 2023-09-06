44d02a12239dcc37dd56fe90202b96c1f5745ae5
JoaoVargas <jotaguivargas@gmail.com>
2023-09-06 17:59:00 -0300

    lista 2 ex1 feita

--- 23.09.04/TesteClasses/src/apresentacao/Principal.java
+++ 23.09.04/TesteClasses/src/apresentacao/Principal.java
@@ -11,24 +11,29 @@ public class Principal {
         Classe2 classe2b = new Classe2(2, classe2a);
         Classe1 classe1a = new Classe1(1, classe2b);
 
-        classe2a.setNum(3);
+        String a = "a";
+        String b = "aaaaab";
 
-        System.out.println(classe1a.getNum());
-        System.out.println(classe1a.getTeste().getNum());
-        System.out.println(classe1a.getTeste().getTeste().getNum());
+        System.out.println(a.compareToIgnoreCase(b));
 
-        classe2a.setTeste(classe2a);
-
-        System.out.println(classe2a.getNum());
-        System.out.println(classe2a.getTeste().getNum());
-        System.out.println(classe2a.getTeste().getTeste().getNum());
-
-        System.out.println(LocalDateTime.now());
-        System.out.println(LocalDateTime.now().getYear());
-
-        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-
-        System.out.println(LocalDateTime.now().format(myFormatObj));
+//        classe2a.setNum(3);
+//
+//        System.out.println(classe1a.getNum());
+//        System.out.println(classe1a.getTeste().getNum());
+//        System.out.println(classe1a.getTeste().getTeste().getNum());
+//
+//        classe2a.setTeste(classe2a);
+//
+//        System.out.println(classe2a.getNum());
+//        System.out.println(classe2a.getTeste().getNum());
+//        System.out.println(classe2a.getTeste().getTeste().getNum());
+//
+//        System.out.println(LocalDateTime.now());
+//        System.out.println(LocalDateTime.now().getYear());
+//
+//        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+//
+//        System.out.println(LocalDateTime.now().format(myFormatObj));
 
 
 
